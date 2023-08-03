392a266584e1bc4b00589d62277ce32fd39a3699
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2023-08-03 14:32:05 +0300

    :construction: models edits

--- User.php
+++ User.php
@@ -5,16 +5,28 @@
         private $userId;
         private $name;
         private $email;
+        private $password;
+        private $phone;
+        private $address;
         // Add other user details
     
-        public function __construct($userId, $name, $email) {
-            $this->userId = $userId;
+        public function __construct($name, $email, $password, $phone, $address) {
             $this->name = $name;
             $this->email = $email;
+            $this->password = $password;
+            $this->phone = $phone;
+            $this->address = $address;
             // Initialize other user details here
         }
-    
+
         // Add getters and setters for properties
+        public function getUserId() {
+            return $this->userId;
+        }
+
+        public function setUserId($userId) {
+            $this->userId = $userId;
+        }
     }
     
 ?>
