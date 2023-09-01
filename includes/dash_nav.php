fe939d7f894e987d7ddb3428126cb19381203390
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2023-09-01 17:42:29 +0300

    :construction: Adding profile link on dashboard

--- includes/dash_nav.php
+++ includes/dash_nav.php
@@ -20,9 +20,11 @@
         </ul>
         <!-- Example split danger button -->
         <div class="btn-group">
+          <a href="profile.php">
           <button type="button" class="btn btn-danger">
-            <img src="./image/profile_image.png" class="profile-img" alt="">Profile
+            <img src="./image/profile_image.png" class="profile-img" alt=""> Profile
           </button>
+          </a>
           <button type="button" class="btn btn-danger dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
             <span class="visually-hidden">Toggle Dropdown</span>
           </button>
