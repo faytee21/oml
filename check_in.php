73efeb3edc01b79956fc87a93df93ac2d22f9572
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2023-09-05 12:25:04 +0300

    :construction: check in proceedure initiation

--- check_in.php
+++ check_in.php
@@ -17,7 +17,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profile</title>
+    <title>Dashboard</title>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
@@ -36,29 +36,7 @@
           include_once "includes/sidenav.php";
         ?>
         <div class="col-md-10">
-            <?php 
-                // User::sessionInfo();
-                $user = User::getUser($_SESSION['id']);
-                // print_r($user);
-            ?>
-            <h2>Profile</h2>
-            <hr>
-            <div class="row">
-                <div class="col-md-6">
-                  <img src="./image/profile_image.png" class="img-fluid" alt="">
-                </div>
-                <div class="col-md-6">
-                 <p><b>Your username:</b> <?php echo $user["username"];?></p>
-                 <p><b>Your email: </b><?php echo $user["email"]; ?></p>
-                </div>
-            </div>
-
-            <div class="main_content">
-              <h2 class="text-center">Complete Profile here</h2>
-              <form action="">
-                <input type="number" class="" placeholder="ID NUMBER">
-              </form>
-            </div>
+          
 
 
         </div>
--- includes/sidenav.php
+++ includes/sidenav.php
@@ -1,7 +1,7 @@
         <div class="col-md-2 bg-dark text-light before-requirements">
           <ul class="requirements">
             <li><a href="dashboard.php">Dashboard</a></li>
-            <li> <a href="">Check In/Check Out</a> </li>
+            <li> <a href="check_in.php">Check In/Check Out</a> </li>
             <li><a href="book_a_room.php">Book A Room</a></li>
             <li> <a href="booked_rooms.php">See Booked Rooms</a></li>
             <li><a href="book_conference_room.php">Book Conference Room/Event</a></li>
